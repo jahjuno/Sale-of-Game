b5908a437a523283e07a356c1e19d7b60b37319c
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2019-10-03 18:37:26 +0300

    regex inscription finished

---
+++ back-office/script/Connect_bdd.php
@@ -0,0 +1,9 @@
+<?php
+class Connect_bdd{
+    protected function dbconnect(){
+        $bdd = new PDO('mysql:host=localhost;dbname=gstore','gaetan','gaetan') or die('not connect');
+        return $bdd;
+    }
+
+}
+?>
