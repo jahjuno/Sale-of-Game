3b7c9517e36f91a4d6d16f25f43c2be9fa0281a6
Landry Manankoraisina RASENDRANIRINA <landry.manankoraisina.rasendranirina@esti.mg>
2019-10-03 10:19:23 +0300

    sidenav demo fini sans js icons

--- public/js/sidenav.js
+++ public/js/sidenav.js
@@ -5,3 +5,21 @@ function openNav() {
       function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
       }
+
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
