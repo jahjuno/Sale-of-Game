95321d2baaeedf9eb46dc23efc919a0b71bcaebc
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2019-10-04 11:17:35 +0300

    se connecter fini + capitaliser le nom

--- back-office/script/service.php
+++ back-office/script/service.php
@@ -11,8 +11,15 @@ if (isset($_POST['inscription'])){
     $query = new Query_bdd();
     echo $query->inscription($nom, $prenom, $mail, $password);
 
+}
 
 
+if (isset($_POST['connexion'])){
+  $mail = $_POST['mail'];
+  $password = $_POST['password'];
+
+  $query = new Query_bdd();
+  echo $query->connexion($mail, $password);
 }
 
 ?>
--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,11 @@
-function capitale(texte) {
-  // atao majuscule premiere lettre 
-    return texte.charAt(0).toUpperCase() + texte.slice(1);
-}
-
-
 function inscription(){
 
-  let nom = $("#inscrire input[name=nom]"); // alaina le champ ana texte ampidirana anarana
-  let prenom = $("#inscrire input[name=prenom]"); // karan'le ambony fogn fa porenom ndrek raika ty
-  let mail = $("#inscrire input[name=mail]"); // mangalaka an le mail nsoratana
-  let pass1 = $("#inscrire input[name=password]");  // l objet html mot de passe
-  let pass2 = $("#inscrire input[name=cpassword]");  // le confirmationy e
-  let error = $("#inscrire p[name=erreur]");
+  var nom = $("#inscrire input[name=nom]"); // alaina le champ ana texte ampidirana anarana
+  var prenom = $("#inscrire input[name=prenom]"); // karan'le ambony fogn fa porenom ndrek raika ty
+  var mail = $("#inscrire input[name=mail]"); // mangalaka an le mail nsoratana
+  var pass1 = $("#inscrire input[name=password]");  // l objet html mot de passe
+  var pass2 = $("#inscrire input[name=cpassword]");  // le confirmationy e
+  var error = $("#inscrire p[name=erreur]");
   if ((/^[a-zA-Z]{3,}$/).test(nom.val())) // verifiena fa lettre ihany no nampidiriny de telo farafahakeliny
   {
     if ((/^[a-zA-Z ]{3,}$/).test(prenom.val())) // verifiena kar le anarana takeo fa afaka asina espace raika ty
@@ -53,8 +47,8 @@ function sInscrire(nom, prenom, mail, password){
     'back-office/script/service.php',
     {
       inscription: 1,
-      nom: nom.toUpperCase(),
-      prenom:capitale(prenom),
+      nom: nom,
+      prenom: prenom,
       mail: mail,
       password: password
     },
@@ -69,38 +63,4 @@ function sInscrire(nom, prenom, mail, password){
         $("#inscrire p[name=erreur]").text(response);
       }
   }
-}
-
-
-function verifie_password(){
-  let mail = $('#connecter input[name=mail]');
-  let password = $('#connecter input[name=password]');
-  let error = $('#connecter p[name=erreur]');
-
-  if (/^[a-zA-Z]{1}[a-zA-Z_.0-9]{1,}@[a-z]{3,}.[a-z]{2,4}$/.test(mail.val())){
-    $.post(
-      'back-office/script/service.php',
-      {
-        connexion: 1,
-        mail: mail.val(),
-        password: password.val()
-      },
-      feed_back
-    );
-
-    function feed_back(rep){
-      if (rep == 1){
-        alert('here');
-        ("#connecter").modal('hide');
-      }
-      else{
-        error.text(rep);
-      }
-    }
-  }
-  else{
-    error.text("Ce mail n'est pas valide: " + mail.val());
-  }
-
-
 }
