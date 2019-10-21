d76d9bff7ba941e03592aee841a2c3cb3b77c8b5
Gaetan Jonathan <gaetan.jonathan.bakary@esti.mg>
2019-10-22 02:11:34 +0300

    Suppression du fichier de connexion a la base de donnée du repertoire git et l'ajout de ce dernier dans le fichier gitignore

--- back-office/script/database.php
+++
@@ -1,8 +0,0 @@
-<?php 
-class Connect_bdd{
-    protected function dbconnect(){
-        $bdd = new PDO('mysql:host=localhost;dbname=GSTORE','sserver','sserver') or die('not connect');
-	return $bdd;
-    }
-}
-?>
