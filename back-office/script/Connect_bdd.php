d1499c4c166a40dd86c59f54edc7771f48c62ccc
Gaetan Jonathan <gaetan.jonathan.bakary@esti.mg>
2019-10-21 22:51:53 +0300

    securisation base de donnée: Separer la classe qui contient le m authe,tification de la base de donnée pour securiser, et de la mettre dans gitignore

--- back-office/script/Connect_bdd.php
+++ back-office/script/Connect_bdd.php
@@ -1,11 +1,5 @@
 <?php
-class Connect_bdd{
-    protected function dbconnect(){
-        $bdd = new PDO('mysql:host=localhost;dbname=gstore','gaetan','gaetan') or die('not connect');
-        return $bdd;
-    }
-
-}
+require('./database.php');
 
 class Query_bdd extends Connect_bdd{
 
